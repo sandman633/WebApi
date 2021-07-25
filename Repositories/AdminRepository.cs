5037046b23228f2f2bcc80013acd0d1a1ea7ee20
sandman633 <69395587+sandman633@users.noreply.github.com>
2021-07-25 23:35:03 +0300

    added repository for admin service

---
+++ Repositories/AdminRepository.cs
@@ -0,0 +1,45 @@
+﻿using Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class AdminRepository : IAdminRepository
+    {
+        public void BanUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<News> CreateNews()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteComment(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteNews(int newsId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditNews(int newsId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<News>> GetNews()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnbanUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repositories/IAdminRepository.cs
@@ -0,0 +1,17 @@
+﻿using Model.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public interface IAdminRepository
+    {
+        public Task<IEnumerable<News>> GetNews();
+        public Task<News> CreateNews();
+        public void EditNews(int newsId);
+        public void DeleteNews(int newsId);
+        public void DeleteComment(int commentId);
+        public void BanUser(int userId);
+        public void UnbanUser(int userId);
+    }
+}
