016d32d79045ff87976805559c08f2b59600b433
sandman633 <69395587+sandman633@users.noreply.github.com>
2021-07-29 00:44:24 +0300

    added mappings

---
+++ Repositories/Mappings/NewsProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Models.Dto;
+using DAL.Domain;
+
+namespace Repositories.Mappings
+{
+    /// <summary>
+    /// Mapping profile for "News" entity.
+    /// </summary>
+    public class NewsProfile : Profile
+    {
+        /// <summary>
+        /// Initializes the instance <see cref="NewsProfile"/>.
+        /// </summary>
+        public NewsProfile()
+        {
+            CreateMap<News, NewsDto>().ReverseMap();
+        }
+    }
+}
---
+++ Repositories/Mappings/RoleProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Domain;
+
+namespace Repositories.Mappings
+{
+    /// <summary>
+    /// Mapping profile for "Role" entity.
+    /// </summary>
+    public class RoleProfile : Profile
+    {
+        /// <summary>
+        /// Initializes the instance <see cref="RoleProfile"/>.
+        /// </summary>
+        public RoleProfile()
+        {
+            CreateMap<Role, RoleDto>().ReverseMap();
+        }
+    }
+
+}
