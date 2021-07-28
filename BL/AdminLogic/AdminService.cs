af48e4ba024fadd62228ac44b066e4d6929d0751
sandman633 <69395587+sandman633@users.noreply.github.com>
2021-07-28 22:34:43 +0300

    update models

--- BL/AdminLogic/AdminService.cs
+++ BL/AdminLogic/AdminService.cs
@@ -1,13 +1,9 @@
 ﻿using AutoMapper;
-using Microsoft.EntityFrameworkCore;
 using DAL.Domain;
 using Repositories;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Models.Response;
+
 
 namespace BL.AdminLogic
 {
--- Model/Domain/User.cs
+++ Model/Domain/User.cs
@@ -6,8 +6,25 @@ namespace DAL.Domain
 {
     public class User : BaseEntity
     {
+        /// <summary>
+        /// Name.
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Surname.
+        /// </summary>
+        public string Surname { get; set; }
+        /// <summary>
+        /// Email.
+        /// </summary>
         public string Email { get; set; }
+        /// <summary>
+        /// Age.
+        /// </summary>
+        public int Age { get; set; }
+        /// <summary>
+        /// Password.
+        /// </summary>
         public string Password { get; set; }
     }
 }
