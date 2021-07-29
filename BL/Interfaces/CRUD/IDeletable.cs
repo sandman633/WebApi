952c406e36999922e8a2b09b6645ac5bd454c347
sandman633 <69395587+sandman633@users.noreply.github.com>
2021-07-29 18:54:41 +0300

    added base service

---
+++ BL/Interfaces/CRUD/IDeletable.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace BL.Interfaces.CRUD
+{
+    /// <summary>
+    /// Interface for deleting entity.
+    /// </summary>
+    public interface IDeletable
+    {
+        /// <summary>
+        /// Interface method for deleting entity.
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <returns></returns>
+        Task DeleteAsync(params int[] ids);
+    }
+}
